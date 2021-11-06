9d9eb1f9f2326860410f203ceb2e69feeca95602
AbediSadegh <sadeghabedi@live.com>
2021-11-06 13:46:53 +0330

    add capacity and mips

--- contracts/Ttc.sol
+++ contracts/Ttc.sol
@@ -1,72 +1,81 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-//TODO: use DDD names
 contract Ttc {
     //TODO: use IteratableMap as library instead of adhoc implementation
-    struct Buyer {
+    struct Tuple {
         uint256 value;
         uint256 ratio;
+        uint256 mips;
         uint256 index;
     }
 
-    struct Seller {
+    struct Server {
         uint256 value;
         uint256 ratio;
+        uint256 capacity;
         uint256 index;
     }
 
-    mapping(address => Seller) public toSeller;
-    mapping(address => Buyer) public toBuyer;
+    mapping(address => Server) public toServer;
+    mapping(address => Tuple) public toTuple;
 
-    Buyer[] public buyers;
-    Seller[] public sellers;
+    Tuple[] public tuples;
+    Server[] public servers;
 
     //FIXME: size of arrays
-    uint256[10][10] public sellersPoints;
-    uint256[10][10] public buyersPoints;
+    uint256[10][10] public serversPoints;
+    uint256[10][10] public tuplesPoints;
 
     error BidValueOrRatioIsZero();
 
-    function bidBuyer(uint256 value, uint256 ratio) external {
-        Buyer memory buyer = toBuyer[msg.sender];
-        if (buyer.value == 0 || buyer.ratio == 0) {
+    function bidTuple(
+        uint256 value,
+        uint256 ratio,
+        uint256 mips
+    ) external {
+        Tuple memory tuple = toTuple[msg.sender];
+        if (tuple.value == 0 || tuple.ratio == 0) {
             if (value == 0 || ratio == 0) {
                 revert BidValueOrRatioIsZero();
             }
-            buyer = Buyer(value, ratio, buyers.length);
-            toBuyer[msg.sender] = buyer;
-            buyers.push(buyer);
+            tuple = Tuple(value, ratio, mips, tuples.length);
+            toTuple[msg.sender] = tuple;
+            tuples.push(tuple);
         }
 
         //FIXME: check points array axis
-        for (uint256 j = 0; j < sellers.length; j++) {
-            buyersPoints[j][buyer.index] = buyer.value * sellers[j].ratio;
+        for (uint256 j = 0; j < servers.length; j++) {
+            tuplesPoints[j][tuple.index] = tuple.value * servers[j].ratio;
         }
 
-        for (uint256 j = 0; j < sellers.length; j++) {
-            sellersPoints[buyer.index][j] = buyer.ratio * sellers[j].value;
+        for (uint256 j = 0; j < servers.length; j++) {
+            serversPoints[tuple.index][j] = tuple.ratio * servers[j].value;
         }
     }
 
-    function bidSeller(uint256 value, uint256 ratio) external {
-        Seller memory seller = toSeller[msg.sender];
-        if (seller.value == 0 || seller.ratio == 0) {
+    function bidServer(
+        uint256 value,
+        uint256 ratio,
+        uint256 capacity
+    ) external {
+        Server memory server = toServer[msg.sender];
+        if (server.value == 0 || server.ratio == 0) {
             if (value == 0 || ratio == 0) {
                 revert BidValueOrRatioIsZero();
             }
-            seller = Seller(value, ratio, sellers.length);
-            toSeller[msg.sender] = seller;
-            sellers.push(seller);
+            server = Server(value, ratio, capacity, servers.length);
+            toServer[msg.sender] = server;
+            servers.push(server);
         }
 
         //FIXME: check points array axis
-        for (uint256 j = 0; j < buyers.length; j++) {
-            sellersPoints[j][seller.index] = seller.value * buyers[j].ratio;
+        for (uint256 j = 0; j < tuples.length; j++) {
+            serversPoints[j][server.index] = server.value * tuples[j].ratio;
         }
 
-        for (uint256 j = 0; j < buyers.length; j++) {
-            buyersPoints[seller.index][j] = seller.ratio * buyers[j].value;
+        for (uint256 j = 0; j < tuples.length; j++) {
+            tuplesPoints[server.index][j] = server.ratio * tuples[j].value;
         }
     }
 }
